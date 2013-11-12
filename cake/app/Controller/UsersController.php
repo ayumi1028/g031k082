1ba4a41d0e52503ab7475cbb3572b2f5fb61ff3e
unknown <g031k082@ISWNI198.ad.iwate-pu.ac.jp>
2013-11-12 14:04:46 +0900

    test

--- cake/app/Controller/UsersController.php
+++ cake/app/Controller/UsersController.php
@@ -7,20 +7,8 @@
 
  	}
  	public function result(){
-		if($this->request->is('POST')){ //POST送信されたかどうか
-			//送信された値を取得
-			//$user = $this->request->data['join'];
-			//ビューに値を受け渡す
-			//$this->set('regist', $user);
-		}else{ //URLで直接アクセスした人など
-			$this->flash(
-				'formアクションから来てください',
-				array(
-					'controller' => 'Touroku',
-					'action' => 'form'
-					)
-				);
-		}
+		if(!$this->request->is('POST')) //URLで直接アクセスした人など
+			$this->flash('formアクションから来てください',array('controller' => 'Touroku','action' => 'form'));
  	}
  	
  }
